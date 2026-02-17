8cf78a048a8fb1f2fd03672f8f08794463df828f
Aineen26 <aineensayyed2005@gmail.com>
2026-02-17 08:32:16 +0530

    User-defined Functions!!

--- src/first_program.py
+++ src/first_program.py
@@ -1,4 +1,4 @@
-def UpLowerCount(str,upper=0,lower=0):
+def lowercase(str,upper=0,lower=0):
     for i in str:
         if i.isupper():
             upper+=1
@@ -9,4 +9,4 @@ def UpLowerCount(str,upper=0,lower=0):
     print(f"Uppercase count: {upper}")
     print(f"Lowercase count: {lower}")
 
-UpLowerCount(str = input("Enter a string: "))
+lowercase(str = input("Enter a string: "))
