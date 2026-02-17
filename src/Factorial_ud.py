11474e71d6a7dc34fbb2ccd48823babddba596c0
Aineen26 <aineensayyed2005@gmail.com>
2026-02-17 08:49:54 +0530

    User-defined Functions!! Factorial Number

---
+++ src/Factorial_ud.py
@@ -0,0 +1,12 @@
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+# print(factorial(int(input("Enter a number: "))))
+
+def fact(n,fact=1):
+    for i in range(1,n+1):
+        fact = fact*i
+    print(fact)
+print(fact(n=int(input("Enter a number: "))))
