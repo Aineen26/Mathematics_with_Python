5ff832d6648c8c2dba58195c1848536be12aa912
Aineen26 <aineensayyed2005@gmail.com>
2026-02-18 22:32:54 +0530

    User-defined Function Prime Number from 1-100

--- src/prime_1-100.py
+++ src/prime_1-100.py
@@ -1,8 +1,17 @@
 """ Program to print the print numbers between 1-100"""
-def Prime(n):
-    if n>1:
-        for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                print(i)
-prime_numbers = [num for num in range(1,101)if Prime(num)]
-print(prime_numbers)
+"""Checks if a number is prime.A prime number must be greater than 1."""
+def is_prime(num):
+    if num <= 1:
+        return False
+    # Check for factors from 2 up to the square root of the number.
+
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False # If divisible, it's not prime
+    return True # If no divisors are found, it is prime
+
+print("Prime numbers from 1 to 100 are:")
+# Loop through numbers in the specified range
+for number in range(1, 101):
+    if is_prime(number):
+        print(number, end=" ")
